09e64dbb691e3daf85595197b610f66eba185e1e
Naiel Fernández <naiel.fdez@gmail.com>
2023-02-16 16:14:19 +0100

    variables declaradas en login y signup

--- app/src/main/java/com/example/buy_sellnow/Login.kt
+++ app/src/main/java/com/example/buy_sellnow/Login.kt
@@ -1,11 +1,23 @@
 package com.example.buy_sellnow
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class Login : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val email: EditText = findViewById(R.id.loginEmailInput);
+        val password: EditText = findViewById(R.id.loginPaaswordInput);
+        val submitBtn: Button = findViewById(R.id.LoginEntrarBtn);
+
+
+
     }
 }
--- app/src/main/java/com/example/buy_sellnow/SignUp.kt
+++ app/src/main/java/com/example/buy_sellnow/SignUp.kt
@@ -1,11 +1,20 @@
 package com.example.buy_sellnow
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class SignUp : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        val email: EditText = findViewById(R.id.signupEmailInput);
+        val password: EditText = findViewById(R.id.signupPaaswordInput);
+        val confirmPassword: EditText = findViewById(R.id.signupConfirmPasswordInput);
+        val submitBtn: Button = findViewById(R.id.signupLoginBtn);
     }
 }
