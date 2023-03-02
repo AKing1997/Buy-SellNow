22cfad4313544cd60a0f080f42ad2fb7b06ffb47
naiel <naiel.fdez@gmail.com>
2023-03-02 18:22:18 +0100

    mejoras chat

--- app/src/main/java/com/example/buy_sellnow/fragments/Chat.kt
+++ app/src/main/java/com/example/buy_sellnow/fragments/Chat.kt
@@ -1,7 +1,19 @@
 package com.example.buy_sellnow.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.buy_sellnow.R
 
 class Chat : Fragment() {
-
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+        var view = inflater.inflate(R.layout.fragment_chat, container, false);
+        return view;
+    }
 }
