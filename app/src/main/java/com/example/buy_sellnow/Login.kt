dc7b68af2fa43cf7f1f31d0bd0cf14dd4789d144
naiel <naiel.fdez@gmail.com>
2023-03-02 17:04:11 +0100

    mejoras login

--- app/src/main/java/com/example/buy_sellnow/Login.kt
+++ app/src/main/java/com/example/buy_sellnow/Login.kt
@@ -7,10 +7,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
-import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignIn.*
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
@@ -18,12 +17,12 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 
 class Login : AppCompatActivity() {
-    private lateinit var auth: FirebaseAuth
+    private lateinit var auth: FirebaseAuth;
     private lateinit var redrige: Intent;
     private lateinit var googleSignInClient : GoogleSignInClient;
     private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
         if (result.resultCode == Activity.RESULT_OK) {
-            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data);
+            val task = getSignedInAccountFromIntent(result.data);
             if (task.isSuccessful) {
                 val account: GoogleSignInAccount? = task.result
                 if (account != null) {
@@ -36,19 +35,19 @@ class Login : AppCompatActivity() {
     }
         @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
         val googleBtn: Button = findViewById(R.id.loginGoogleBtn);
         val email: EditText = findViewById(R.id.loginEmailInput);
         val password: EditText = findViewById(R.id.loginPaaswordInput);
         val submitBtn: Button = findViewById(R.id.LoginEntrarBtn);
         val signUpRedirect: Button = findViewById(R.id.signUpRedirect);
-        auth = FirebaseAuth.getInstance()
+        auth = FirebaseAuth.getInstance();
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
-            .build()
-        googleSignInClient = GoogleSignIn.getClient(this,gso);
+            .build();
+        googleSignInClient = getClient(this,gso);
             googleBtn.setOnClickListener {
                 singInGoogle();
             }
@@ -64,8 +63,8 @@ class Login : AppCompatActivity() {
     }
 
     fun singInGoogle(){
-        val signInIntent = googleSignInClient.signInIntent
-        launcher.launch(signInIntent)
+        val signInIntent = googleSignInClient.signInIntent;
+        launcher.launch(signInIntent);
     }
 
     private fun updateUI(account: GoogleSignInAccount){
@@ -76,15 +75,15 @@ class Login : AppCompatActivity() {
 
                 startActivity(redrige);
             }else{
-                Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show();
             }
         }
     }
 
     override fun onStart() {
-        super.onStart()
+        super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
-        val account = GoogleSignIn.getLastSignedInAccount(this)
+        val account = getLastSignedInAccount(this);
         if(account!=null) {
             updateUI(account);
         }
