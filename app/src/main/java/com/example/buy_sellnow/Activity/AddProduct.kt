d720356515d350b96dc900ac7e014b3b5888ae69
naiel <naiel.fdez@gmail.com>
2023-03-02 19:27:03 +0100

    toolbar

--- app/src/main/java/com/example/buy_sellnow/Activity/AddProduct.kt
+++ app/src/main/java/com/example/buy_sellnow/Activity/AddProduct.kt
@@ -2,11 +2,20 @@ package com.example.buy_sellnow.Activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import com.example.buy_sellnow.R
 
 class AddProduct : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_product)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowHomeEnabled(true)
+        }
     }
+
 }
