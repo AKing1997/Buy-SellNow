a26a9ebb0297ccb2513997cb2496620a3de17ce8
naiel <naiel.fdez@gmail.com>
2023-03-02 18:57:11 +0100

    añadiendo addForm

--- app/src/main/java/com/example/buy_sellnow/MainActivity.kt
+++ app/src/main/java/com/example/buy_sellnow/MainActivity.kt
@@ -1,22 +1,30 @@
 package com.example.buy_sellnow
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Button
 import androidx.fragment.app.Fragment
-import com.example.buy_sellnow.fragments.Chat
-import com.example.buy_sellnow.fragments.Favorite
-import com.example.buy_sellnow.fragments.Home
-import com.example.buy_sellnow.fragments.Profile
+import com.example.buy_sellnow.Activity.AddProduct
+import com.example.buy_sellnow.fragments.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         defaultPage();
         // Creando una variable para los botones de menu navegacion
         val bottomNav: BottomNavigationView = findViewById(R.id.bottomNavigationView);
+        val addForm: Button = findViewById(R.id.addProduct)
+
+        addForm.setOnClickListener {
+            val redrige: Intent = Intent(this, AddProduct::class.java);
+            startActivity(redrige);
+        }
         bottomNav.setOnItemSelectedListener { item: MenuItem ->
             // condicione como Switch
             when (item.itemId) {
