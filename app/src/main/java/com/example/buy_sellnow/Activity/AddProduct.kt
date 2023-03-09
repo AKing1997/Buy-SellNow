c0bacb7c4a126b68b9786d4926838199618ae7f8
naiel <naiel.fdez@gmail.com>
2023-03-09 18:44:42 +0100

    form product a medias

--- app/src/main/java/com/example/buy_sellnow/Activity/AddProduct.kt
+++ app/src/main/java/com/example/buy_sellnow/Activity/AddProduct.kt
@@ -9,13 +9,5 @@ class AddProduct : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_product)
-        val toolbar: Toolbar = findViewById(R.id.toolbar)
-        setSupportActionBar(toolbar)
-
-        supportActionBar?.apply {
-            setDisplayHomeAsUpEnabled(true)
-            setDisplayShowHomeEnabled(true)
-        }
     }
-
 }
