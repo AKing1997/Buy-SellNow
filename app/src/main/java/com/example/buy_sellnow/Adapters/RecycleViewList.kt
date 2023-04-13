554843bdb805285bbf666300b2e2e3171cd810a6
CfAnkit <cf21ankit.kumar@iesjoandaustria.org>
2023-04-13 11:31:28 +0200

    Nuevos Cambios

--- app/src/main/java/com/example/buy_sellnow/Adapters/RecycleViewList.kt
+++ app/src/main/java/com/example/buy_sellnow/Adapters/RecycleViewList.kt
@@ -1,6 +1,7 @@
 package com.example.buy_sellnow.Adapters
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Intent
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -8,12 +9,17 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.buy_sellnow.Activity.ChatDetail
+import com.example.buy_sellnow.Activity.ProductDetail
 import com.example.buy_sellnow.Model.Chat
 import com.example.buy_sellnow.Model.GridViewProduct
 import com.example.buy_sellnow.Model.Product
 import com.example.buy_sellnow.R
+import com.google.firebase.auth.FirebaseAuth
 //import com.bumptech.glide.Glide
 //import es.kankit.camkit.DB.DBHelper
 //import es.kankit.camkit.Interficie.CryptoGecko
@@ -23,6 +29,8 @@ class RecycleViewList(
     val chatList: ArrayList<Product>,
     val context: Context?,
 ) : RecyclerView.Adapter<RecycleViewList.ViewHolder>() {
+    val userId = FirebaseAuth.getInstance().currentUser!!.uid
+
     // Return cantidad de items en el recycler/lista
     override fun getItemCount(): Int {
         return chatList.size;
@@ -32,6 +40,18 @@ class RecycleViewList(
     override fun onBindViewHolder(holder: ViewHolder, @SuppressLint("RecyclerView") position: Int) {
         Glide.with(context!!).load(chatList[position].image[0]).into(holder.img)
         holder.title.text = chatList[position].tituloDeProducto
+
+        holder.chatItemR.setOnClickListener {
+            if(ProductDetail.producto !=null){
+                ProductDetail.redrige = Intent(context, ChatDetail::class.java);
+                ProductDetail.redrige.putExtra("PRODUCT_ID",  chatList[position].productId)
+                ProductDetail.redrige.putExtra("USER_ID", userId)
+                ProductDetail.redrige.putExtra("PRODUCT_TITLE",  chatList[position].tituloDeProducto)
+                ProductDetail.redrige.putExtra("PRODUCT_IMG",  chatList[position].image[0])
+                ProductDetail.redrige.putExtra("PRODUCT_USER_ID",  chatList[position].userId)
+                context.startActivity(ProductDetail.redrige)
+            }
+        }
     }
 
     //Devueve el item que voy a mostrar en el recycler
@@ -44,5 +64,6 @@ class RecycleViewList(
     class ViewHolder(view: View): RecyclerView.ViewHolder(view){
         var img: ImageView = view.findViewById(R.id.chatProdctImage);
         var title: TextView = view.findViewById(R.id.chatProductName);
+        var chatItemR: CardView = view.findViewById(R.id.chatItemR)
     }
 }
